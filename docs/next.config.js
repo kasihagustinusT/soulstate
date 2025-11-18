e6bad989861f52bb13d0a3d8979f67a182d5a725
Kasih Agustinus Tampubolon <kasihagustinus22@gmail.com>
2025-11-18 12:36:01 +0700

    Update next.config.js

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,5 +1,6 @@
-import createMDX from '@next/mdx';
-import remarkGfm from 'remark-gfm';
+// next.config.js
+const createMDX = require('@next/mdx');
+const remarkGfm = require('remark-gfm');
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
@@ -12,18 +13,13 @@ const withMDX = createMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = withMDX({
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-
-  // required untuk deployment Vercel
   output: 'standalone',
-
   images: {
     unoptimized: true
   },
-
-  // NON-AKTIFKAN MDX-RS (ini penyebab Next pakai import-source-file)
   experimental: {
     mdxRs: false
   }
 });
 
-export default nextConfig;
+module.exports = nextConfig;
